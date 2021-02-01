0a04a2a3ac627e3b476bcd23ef68d89672bc3789
wangzhilin <41288159@qq.com>
2021-02-01 19:41:28 +0800

    开始用户详情界面

--- app/src/main/java/wzl/android/ducksplash/ui/UserFragment.kt
+++ app/src/main/java/wzl/android/ducksplash/ui/UserFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import wzl.android.ducksplash.R
 import wzl.android.ducksplash.databinding.FragmentUserBinding
+import wzl.android.ducksplash.util.loadCirclePhotoUrl
 import wzl.android.ducksplash.util.reserveStatusBar
 
 /**
@@ -15,6 +18,7 @@ import wzl.android.ducksplash.util.reserveStatusBar
  */
 class UserFragment: Fragment() {
 
+    private val args by navArgs<UserFragmentArgs>()
     private lateinit var viewBinding: FragmentUserBinding
 
     override fun onCreateView(
@@ -28,6 +32,17 @@ class UserFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewBinding.appBarLayout.reserveStatusBar()
+        viewBinding.apply {
+            appBarLayout.reserveStatusBar()
+            val url = args.user?.profileImage?.large?:""
+            userInfoLayout.userHead.loadCirclePhotoUrl(url, R.drawable.drawable_circle_image_placeholder)
+            val userName = if (args.user == null) "" else "${args.user?.firstName} ${args.user?.lastName?:""}"
+            userInfoLayout.userName.text = userName
+            userInfoLayout.location.text = args.user?.location
+            userInfoLayout.photoCount.text = "${args.user?.totalPhotos}"
+            userInfoLayout.favoriteCount.text = "${args.user?.totalLikes}"
+            userInfoLayout.collectionCount.text = "${args.user?.totalCollections}"
+            toolBar.title = args.user?.username ?: ""
+        }
     }
 }
