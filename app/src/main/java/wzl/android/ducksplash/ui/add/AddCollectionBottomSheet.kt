5e4ca53766b0f4f7ec5d0e029634366ae17bcfa4
wangzhilin <41288159@qq.com>
2021-03-17 19:26:31 +0800

    准备添加 ‘markbook’ 功能

---
+++ app/src/main/java/wzl/android/ducksplash/ui/add/AddCollectionBottomSheet.kt
@@ -0,0 +1,12 @@
+package wzl.android.ducksplash.ui.add
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ *Created on 2021/3/17
+ *@author zhilin
+ */
+class AddCollectionBottomSheet : BottomSheetDialogFragment() {
+
+
+}
